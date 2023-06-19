3f3606d05be58192ccbcac73c2a993435cac2f30
Matheus Duarte Albuquerque <90364847+Mathdal002@users.noreply.github.com>
2023-06-19 17:51:30 -0300

    Add files via upload

---
+++ curso Secao10 Vetor Fixacao/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Aluguel;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Aluguel[] vect = new Aluguel[10];
+		
+		System.out.print("How many rooms will be rented?");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i<=n; i++) {
+			System.out.println();
+			System.out.println("Aluguel #" + i + ":");
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			vect[room] = new Aluguel(name, email);
+		}
+		
+		System.out.println();
+		System.out.println("Busy rooms:");
+		for(int i=0; i<10; i++) {
+			if(vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+	}
+}
